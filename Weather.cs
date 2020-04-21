b71905c5cd7904bd41f918f18137bda4020bc16c
Lluminatarion-8431 <captain.a.8431@gmail.com>
2020-04-20 22:38:43 -0700

    Added two random generators to give both the temp. and details of the day.

--- Weather.cs
+++ Weather.cs
@@ -8,21 +8,41 @@ namespace LemonadeStand_3DayStarter
 {
     class Weather
     {
-        //Member Variables//
+        //Member Variables// 
         protected double temperature;
-        protected string percipitation;
+        protected string atmosphere;
         protected double demandLevel;
-
+        protected double sunnyFactor = 1;
+        protected double rainyFactor = .5;
 
 
         //Contstructor//
+
+        //generates a random double int to simulate temperature for the day
+        Random temp = new Random();
+        this.temperature = temp.Next(65, 110);
+        //generates a random number for the atmosphere
         Random rnd = new Random();
-        this.temperature = rnd.Next(65, 110);
+        int atmos = rnd.Next(1, 3);
 
+        //Member Methods//
 
-
-
-        //Member Methods
+        //this will give value to Random(1, 3) to help generate the atomsphere of the day.
+        public Weather()
+        {
+            if (atmos == 1)
+            {
+                this.atmosphere = "Sunny";
+            }
+            else if (atmos == 2)
+            {
+                this.atmosphere = "Cloudy";
+            }
+            else
+            {
+                this.atmosphere = "Rainy";
+            }
+        }
 
         // weather conditon could affect how many customers come for the day 
         //according to temp. if they buy or not; 
